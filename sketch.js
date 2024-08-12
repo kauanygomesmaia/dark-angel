53fdb6365d2a4a81916cf5bf291043d93cdff7f9
kauanygomesmaia <kauany.gomes.maia@escola.pr.gov.br>
2024-08-12 09:46:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() 
+    background(0);
+    circle(xBolinha, yBolinha, diametro);
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+    createCanvas(600, 400);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
